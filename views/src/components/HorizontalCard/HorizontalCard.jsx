12a05b01099859719a175920e8cf651ec8ebfade
AlejandraGF <agfp108@gmail.com>
2024-05-17 23:06:21 -0600

    Comunidades card multiple

--- views/src/components/HorizontalCard/HorizontalCard.jsx
+++ views/src/components/HorizontalCard/HorizontalCard.jsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './style.css'; 
+import './style.css';
 
-
-  
 const HorizontalCard = ({ image, title, description, link }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleNextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % image.length);
+  };
+
+  const handlePrevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? image.length - 1 : prevIndex - 1
+    );
+  };
+
+  // Check if images array is empty or undefined
+  if (!image || image.length === 0) {
+    return (
+      <div className="card">
+        <div className="card-content">
+          <h2 className="card-title">{title}</h2>
+          <p className="card-description">{description}</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <Link to={link} style={{ textDecoration: 'none' }}>
       <div className="card">
-        <img src={image} alt={title} className="card-image" />
+        <div className="image-container">
+          <img
+            src={image[currentImageIndex]}
+            alt={title}
+            className="card-image"
+          />
+          <div className="buttons-container">
+          <button className="prev-button" onClick={handlePrevImage}>
+            &lt;
+          </button>
+          <button className="next-button" onClick={handleNextImage}>
+            &gt;
+          </button>
+          </div>
+        </div>
         <div className="card-content">
           <h2 className="card-title">{title}</h2>
           <p className="card-description">{description}</p>
@@ -19,4 +55,3 @@ const HorizontalCard = ({ image, title, description, link }) => {
 };
 
 export default HorizontalCard;
-  
