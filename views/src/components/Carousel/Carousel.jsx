c08eaf38244762a06207a6dbf28caeee12492e35
Deif <davidsuarez2001@gmail.com>
2024-05-24 17:44:16 -0600

    Se agrego el manejo del User Loggeado
    El User Loggeado se maneja correctamente en el header y se conoce en el resto del sistema.

--- views/src/components/Carousel/Carousel.jsx
+++ views/src/components/Carousel/Carousel.jsx
@@ -29,7 +29,7 @@ const Carousel = ({cards, images}) => {
       <div className="carousel-container">
         <div className="carousel">
           {cards && slides && cards.slice(currentCard, currentCard + 3).map((card, index) => {
-            // Filtrar slides por el valor de "community" correcto
+            // Filtra slides por el valor de "community" correcto
             const cardCommunity = currentCard + index + 1;
             const filteredSlides = slides ? slides.filter(slide => slide.community === cardCommunity) : [];
   
--- views/src/components/Header/Header.jsx
+++ views/src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
+import { useNavigate } from "react-router-dom";
+import { useGet } from '../../useGet';
 import logo from "../../imgs/logo.png";
 
 function Header() {
+  // GET Users
+  const {variable: users} = useGet('http://localhost:4000/api/usuario');
+  const userId = sessionStorage.getItem('userId');
+
+  let navigate = useNavigate();
+  const handleLogout = () => {
+    sessionStorage.setItem('userId', -1);
+    navigate('/');
+  };
+
   return (
     <header className="header">
       <div className="logo">
@@ -17,7 +29,21 @@ function Header() {
         <Link to="/publicaciones">Publicaciones</Link>
         <Link to="/donaciones">Donaciones</Link>
         <Link to="/comunidades">Comunidades</Link>
-        <Link to="/login">Iniciar Sesión</Link>    
+
+        {userId === null || userId == "-1" ? (
+          <Link to="/login">Iniciar Sesión</Link>
+        ) : (
+          <div className="user-menu">
+            {users && (
+              <span className="user-name">
+                {users.find(user => user.id === parseInt(userId, 10))?.name}
+              </span>
+            )}
+            <div className="dropdown-menu">
+              <button onClick={handleLogout}>Cerrar Sesión</button>
+            </div>
+          </div>
+        )}
       </nav>
     </header>
   );
