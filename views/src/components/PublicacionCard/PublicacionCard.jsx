74ed98144b896e66cfa27b47db4c2efbe10f41a1
AlejandraGF <agfp108@gmail.com>
2024-05-24 23:41:31 -0600

    Publication cards created

--- views/src/components/PublicacionCard/PublicacionCard.jsx
+++ views/src/components/PublicacionCard/PublicacionCard.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
+import Modal from '../Modal/Modal.jsx';
+
 
 const PublicacionCard = ({ images, title, description, link }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const nextImage = () => {
     setCurrentImageIndex((prevIndex) =>
@@ -17,19 +20,41 @@ const PublicacionCard = ({ images, title, description, link }) => {
     );
   };
 
+  const truncatedDescription = description.length > 50 
+    ? `${description.substring(0, 100)}...` 
+    : description;
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+
   return (
-    <div className="pub-card">
+    <>
+    <div className="pub-card" onClick={openModal}>
       <div className="pub-image-container">
-        <button className="pub-nav-button left" onClick={prevImage}>◄</button>
+        <button className="pub-nav-button left"  onClick={(e) => { e.stopPropagation(); prevImage(); }}>◄</button>
         <img src={images[currentImageIndex]} alt="Card" className="pub-image"/>
-        <button className="pub-nav-button right" onClick={nextImage}>►</button>
+        <button className="pub-nav-button right" onClick={(e) => { e.stopPropagation(); nextImage(); }}>►</button>
       </div>
       <div className="pub-card-content">
         <h2>{title}</h2>
-        <p>{description}</p>
-        <button className="pub-see-more-button">Ver más</button>
+        <p>{truncatedDescription}</p>
+        <button className="pub-see-more-button" onClick={(e) => { e.stopPropagation(); openModal(); }}>Ver más</button>
       </div>
     </div>
+          <Modal 
+          isOpen={isModalOpen} 
+          onClose={closeModal} 
+          images={images} 
+          title={title} 
+          description={description} 
+        />
+      </>
   );
 };
 export default PublicacionCard;
